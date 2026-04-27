f49e76203249cfbeb462ec673d42b13e6f873308
Naruto-Uzumaki-Yt <narutouzumaki3016day@gmail.com>
2026-04-27 20:22:56 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -170,9 +170,9 @@ async def save_media(client, message: Message):
 
     # Detect file type
     if message.video:
-    file = message.video
-    file_type = "video"
-    thumb = file.thumbs[-1].file_id if file.thumbs else None
+        file = message.video
+        file_type = "video"
+        thumb = file.thumbs[-1].file_id if file.thumbs else None
 
     elif message.audio:
         file = message.audio
